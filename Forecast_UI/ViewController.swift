e033c032c6dd2f298b38a89679a46b90400c5408
F4rab1 <issafarabi2022@gmail.com>
2023-05-01 20:10:03 +0600

    Initial Commit

---
+++ Forecast_UI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Forecast_UI
+//
+//  Created by Фараби Иса on 01.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
